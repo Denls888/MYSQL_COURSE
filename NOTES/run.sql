8744726dea47314211a579731ccd959675fcd15e
Denls888 <hackintosh95@gmail.com>
2021-03-15 21:49:22 -0700

    kkk

--- NOTES/run.sql
+++ NOTES/run.sql
@@ -10,12 +10,37 @@
 
 --SELECT CONCAT('Hellow', 'world');
 
-SELECT
-   CONCAT(author_fname, ' ', author_lname)
+SELECT REVERSE('Denis Isaev');
+
+SELECT 
+   REPLACE(title, ' ', '->') AS 'title' 
 FROM books;
 
-SELECT author_fname AS first, author_lname AS last,
-CONCAT (author_fname, ' ', author_lname) AS full
+SELECT 
+   author_fname AS 'forwards',
+   REVERSE(author_fname) AS 'backwards'
 FROM books;
+
+SELECT
+   UPPER(concat(author_fname, ' ', author_lname))
+   AS 'full name in caps'
+FROM books;
+
+SELECT 
+   CONCAT(title, ' was relised in ', released_year)
+   AS blurb
+FROM books;
+
+SELECT
+   title,
+   CHAR_LENGTH(title) AS 'char count'
+FROM books;
+
+SELECT
+   CONCAT(SUBSTRING(title, 1, 10), '...') AS 'short title',
+   CONCAT(author_lname, ', ', author_fname) AS 'author',
+   CONCAT(stock_quantity, ' in stock') AS 'quantity'
+FROM books LIMIT 2;
+
 
 
