3fd18d0a8efe5baa7f092a18beeb354152b71322
Denls888 <hackintosh95@gmail.com>
2021-03-22 21:47:43 -0700

    DONE!!!

--- NOTES/rlshps_and_joints.sql
+++ NOTES/rlshps_and_joints.sql
@@ -210,9 +210,13 @@ FROM   customers AS c
                ON c.id = o.customer_id
 GROUP  BY NAME
 ORDER  BY amount DESC; 
-
-
-
++----------------+-------------+
+| Name           | Total spent |
++----------------+-------------+
+| George Michael | 813.17      |
+| Bette Davis    | 450.25      |
+| Boy George     | 135.49      |
++----------------+-------------+
 
 /* -------------------------------------------------------------------------- */
 /* ON DELETE CASCADE                                                          */
@@ -234,7 +238,7 @@ CREATE TABLE orders(
     customer_id INT,
     FOREIGN KEY(customer_id) 
         REFERENCES customers(id)
-        ON DELETE CASCADE
+        ON DELETE CASCADE         -- so we can delete table 'customers'
 );
 --#####################################################
 
@@ -297,10 +301,172 @@ ORDER BY
 
 
 
+/* -------------------------------------------------------------------------- */
+/* challenges                                                                 */
+/* -------------------------------------------------------------------------- */
 
 
+-- challenge 1
+SELECT
+	title,
+	rating
+FROM series
+JOIN reviews 
+	ON series.id = reviews.series_id 
+LIMIT 10;
 
 
+-- challenge 2
+SELECT
+	title,
+	AVG(rating) AS 'avg_rating'
+FROM series
+JOIN reviews 
+	ON series.id = reviews.series_id
+GROUP BY series.id
+ORDER BY avg_rating ASC;
+
+title               |avg_rating|
+--------------------|----------|
+General Hospital    |   5.38000|
+Bobs Burgers       |   7.52000|
+Seinfeld            |   7.60000|
+Bojack Horseman     |   7.94000|
+Arrested Development|   8.08000|
+
+
+-- challenge 3
+SELECT first_name,
+	   last_name,
+	   rating
+FROM reviewers  -- since it's inner join these can be changed places
+JOIN reviews 
+	ON reviewers.id = reviews.reviewer_id
+LIMIT 5;
+
+first_name|last_name|rating|
+----------|---------|------|
+Thomas    |Stoneman |   8.0|
+Thomas    |Stoneman |   8.1|
+Thomas    |Stoneman |   7.0|
+Thomas    |Stoneman |   7.5|
+Thomas    |Stoneman |   9.5|
+
+-- challenge 4
+SELECT title AS 'UNRATED'
+from series
+LEFT JOIN reviews
+	ON series.id = reviews.series_id
+WHERE rating IS NULL;
+_____________________________
+UNRATED              |
+---------------------|
+Malcolm In The Middle|
+Pushing Daisies      |
+
+
+-- challenge 5
+SELECT genre,
+	   ROUND(AVG(rating), 2) AS avg_rating
+from series 
+INNER JOIN reviews                       -- we do not want to work with not exist rating ;(
+	ON series.id = reviews.series_id 
+GROUP BY genre;
+
+genre    |avg_rating|
+---------|----------|
+Animation|      7.86|
+Comedy   |      8.16|
+Drama    |      8.04|
+
+-- challenge 6
+SELECT first_name,
+       last_name,
+       COUNT(rating) AS 'COUNT',
+       IFNULL(MIN(rating), 0) AS 'MIN',
+       IFNULL(MAX(rating), 0) AS 'MAX',
+       ROUND(IFNULL(AVG(rating), 0), 2) AS 'AVG',
+CASE 
+	WHEN COUNT(rating) >= 1 THEN 'ACTIVE'
+	ELSE 'INACTIVE'							-- can also be done like -> IF(COUNT(rating) >= 1, 'ACTIVE', 'INACTIVE') AS STATUS
+END AS 'STATUS'
+FROM reviewers
+LEFT JOIN reviews 
+	ON reviewers.id = reviews.reviewer_id
+GROUP BY reviewers.id;
+
+first_name|last_name|COUNT|MIN|MAX|AVG |STATUS  |
+----------|---------|-----|---|---|----|--------|
+Thomas    |Stoneman |    5|7.0|9.5|8.02|ACTIVE  |
+Wyatt     |Skaggs   |    9|5.5|9.3|7.80|ACTIVE  |
+Kimbra    |Masters  |    9|6.8|9.0|7.99|ACTIVE  |
+Domingo   |Cortes   |   10|5.8|9.1|7.83|ACTIVE  |
+Colt      |Steele   |   10|4.5|9.9|8.77|ACTIVE  |
+Pinkie    |Petit    |    4|4.3|8.8|7.25|ACTIVE  |
+Marlon    |Crafford |    0|0.0|0.0|0.00|INACTIVE|
+
+-- challenge 7
+
+SELECT
+	title,
+	rating,
+	CONCAT(first_name, ' ', last_name) AS 'reviewer'
+FROM reviewers
+INNER JOIN reviews 
+	ON reviewers.id = reviews.reviewer_id
+INNER JOIN series
+	ON series.id = reviews.series_id
+ORDER BY title;
+
+title               |rating|reviewer       |
+--------------------|------|---------------|
+Archer              |   8.0|Thomas Stoneman|
+Archer              |   7.7|Domingo Cortes |
+Archer              |   8.5|Kimbra Masters |
+Archer              |   7.5|Wyatt Skaggs   |
+Archer              |   8.9|Colt Steele    |
+Arrested Development|   8.1|Thomas Stoneman|
+Arrested Development|   6.0|Domingo Cortes |
+Arrested Development|   8.0|Kimbra Masters |
+Arrested Development|   8.4|Pinkie Petit   |
+Arrested Development|   9.9|Colt Steele    |
+Bob s Burgers       |   7.5|Pinkie Petit   |
+Bob's Burgers       |   8.0|Colt Steele    |
+Bob's Burgers       |   7.0|Thomas Stoneman|
+Bob's Burgers       |   8.0|Domingo Cortes |
+Bob's Burgers       |   7.1|Kimbra Masters |
+Bojack Horseman     |   7.5|Thomas Stoneman|
+Bojack Horseman     |   8.3|Domingo Cortes |
+Bojack Horseman     |   7.8|Kimbra Masters |
+Bojack Horseman     |   8.5|Colt Steele    |
+Bojack Horseman     |   7.6|Wyatt Skaggs   |
+Breaking Bad        |   9.5|Thomas Stoneman|
+Breaking Bad        |   9.1|Domingo Cortes |
+Breaking Bad        |   9.0|Kimbra Masters |
+Breaking Bad        |   9.9|Colt Steele    |
+Breaking Bad        |   9.3|Wyatt Skaggs   |
+Curb Your Enthusiasm|   6.5|Wyatt Skaggs   |
+Curb Your Enthusiasm|   8.4|Wyatt Skaggs   |
+Curb Your Enthusiasm|   8.8|Domingo Cortes |
+Curb Your Enthusiasm|   7.8|Kimbra Masters |
+Curb Your Enthusiasm|   9.1|Colt Steele    |
+Fargo               |   9.1|Wyatt Skaggs   |
+Fargo               |   9.7|Colt Steele    |
+Freaks and Geeks    |   9.3|Colt Steele    |
+Freaks and Geeks    |   8.8|Pinkie Petit   |
+Freaks and Geeks    |   8.5|Domingo Cortes |
+Freaks and Geeks    |   7.8|Wyatt Skaggs   |
+General Hospital    |   5.5|Wyatt Skaggs   |
+General Hospital    |   4.5|Colt Steele    |
+General Hospital    |   4.3|Pinkie Petit   |
+General Hospital    |   6.8|Kimbra Masters |
+General Hospital    |   5.8|Domingo Cortes |
+Halt and Catch Fire |   9.9|Colt Steele    |
+Seinfeld            |   7.2|Domingo Cortes |
+Seinfeld            |   8.0|Kimbra Masters |
+Stranger Things     |   8.9|Kimbra Masters |
+Stranger Things     |   8.5|Wyatt Skaggs   |
+Stranger Things     |   8.9|Domingo Cortes |
 
 
 
